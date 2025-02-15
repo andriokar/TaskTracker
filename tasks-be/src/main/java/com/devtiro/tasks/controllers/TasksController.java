5ded69813b953a449175606b071aa0d34e6ad960
Andriy <karpus.andrii@proton.me>
2025-02-15 22:56:56 +0100

    Implement List Task

---
+++ tasks-be/src/main/java/com/devtiro/tasks/controllers/TasksController.java
@@ -0,0 +1,35 @@
+package com.devtiro.tasks.controllers;
+
+import com.devtiro.tasks.domain.dto.TaskDto;
+import com.devtiro.tasks.mappers.TaskMapper;
+import com.devtiro.tasks.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/task-lists/{task_list_id}/tasks")
+public class TasksController {
+
+    private final TaskService taskService;
+    private final TaskMapper taskMapper;
+
+    @Autowired
+    public TasksController(TaskService taskService, TaskMapper taskMapper) {
+        this.taskService = taskService;
+        this.taskMapper = taskMapper;
+    }
+
+    @GetMapping
+    public List<TaskDto> listTasks(@PathVariable("task_list_id") UUID taskListId) {
+        return taskService.listTasks(taskListId)
+                .stream()
+                .map(taskMapper::toDto)
+                .toList();
+    }
+}
---
+++ tasks-be/src/main/java/com/devtiro/tasks/services/impl/TaskServiceImpl.java
@@ -0,0 +1,26 @@
+package com.devtiro.tasks.services.impl;
+
+import com.devtiro.tasks.domain.entities.Task;
+import com.devtiro.tasks.repositories.TaskRepository;
+import com.devtiro.tasks.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public List<Task> listTasks(UUID taskListId) {
+        return taskRepository.findByTaskListId(taskListId);
+    }
+}
